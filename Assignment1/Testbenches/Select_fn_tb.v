8c319a0996aec316c6f567c1509695481c2557d9
mcnallc2 <mcnallc2@tcd.ie>
2019-03-13 20:28:13 +0000

    finished version

--- Assignment1/Testbenches/Select_fn_tb.v
+++ Assignment1/Testbenches/Select_fn_tb.v
@@ -3,48 +3,26 @@
 // the simulation timestep is 10 ps
 `timescale 1 ns/10 ps 
 
-module ALU_tb;
+//this test bench is used to test the accuray of the my function selection module.
+//it should take the indivual results of each 8 operations a function number.
+//it should then 
+module Funtion_Sel_tb;
 
    // signal declaration
-   // 
-
-   
-   reg [5:0] test_in0;
-   reg [5:0] test_in1;
-   reg [5:0] test_in2;
-   reg [5:0] test_in3;
-   reg [5:0] test_in4;
-   reg [5:0] test_in5;
-   reg [5:0] test_in6;
-   reg [5:0] test_in7;
-
+   reg [5:0] test_in0, test_in1, test_in2, test_in3, test_in4, test_in5, test_in6, test_in7;
    reg [2:0] fxn;
-   
    wire [5:0] final_res;
+   integer i;
 
-   // instantiate the circuit under 
+   // instantiate the Select_fn module
    Select_fn uut
         (.res0(test_in0), .res1(test_in1), .res2(test_in2), .res3(test_in3), .res4(test_in4), .res5(test_in5), .res6(test_in6), .res7(test_in7), .final(final_res), .func(fxn));
 
    //  test vector generator
    initial
    begin
-      // test vector 1
-      //A
-      fxn = 3'b000;
-      test_in0 = 6'b000000;
-      test_in1 = 6'b000001;
-      test_in2 = 6'b000010;
-      test_in3 = 6'b000011;
-      test_in4 = 6'b000100;
-      test_in5 = 6'b000101;
-      test_in6 = 6'b000110;
-      test_in7 = 6'b000111;
-      # 100;
       
-            // test vector 1
-      //A
-      fxn = 3'b010;
+      //these are the input test vectors representing the results from each operation
       test_in0 = 6'b000000;
       test_in1 = 6'b000001;
       test_in2 = 6'b000010;
@@ -53,31 +31,16 @@ module ALU_tb;
       test_in5 = 6'b000101;
       test_in6 = 6'b000110;
       test_in7 = 6'b000111;
-      # 100;
-            // test vector 1
-      //A
-      fxn = 3'b100;
-      test_in0 = 6'b000000;
-      test_in1 = 6'b000001;
-      test_in2 = 6'b000010;
-      test_in3 = 6'b000011;
-      test_in4 = 6'b000100;
-      test_in5 = 6'b000101;
-      test_in6 = 6'b000110;
-      test_in7 = 6'b000111;
-      # 100;
-            // test vector 1
-      //A
-      fxn = 3'b110;
-      test_in0 = 6'b000000;
-      test_in1 = 6'b000001;
-      test_in2 = 6'b000010;
-      test_in3 = 6'b000011;
-      test_in4 = 6'b000100;
-      test_in5 = 6'b000101;
-      test_in6 = 6'b000110;
-      test_in7 = 6'b000111;
-      # 100;
+      //the inital function is 000
+      fxn = 3'b000;
+      
+      //this loops through testing fro all 8 functions.
+      for(i=0; i<8; i=i+1)
+        begin
+        #100;
+        //increments to the next operation.
+        fxn = fxn + 3'b001;
+        end
 
       // stop simulation 
       $stop;
--- Assignment1/Testbenches/xor_testbench.v
+++ Assignment1/Testbenches/xor_testbench.v
@@ -3,10 +3,11 @@
 // the simulation timestep is 10 ps
 `timescale 1 ns/10 ps 
 
+//this module is used to test the 6-bit XOR feature
 module Ripple_XOR_tb;
 
    //two 6-bit inputs
-   //6-bit output representing OR value
+   //6-bit output representing XOR value
    reg  [5:0] test_in0, test_in1;
    wire [5:0] test_out;
 
@@ -18,41 +19,49 @@ module Ripple_XOR_tb;
    initial
    begin
       // test vector 1
+      //all values are zero
       test_in0 = 6'b000000;
       test_in1 = 6'b000000;
       # 100;
       
       // test vector 2
+      //A is larger than B
       test_in0 = 6'b001010;
       test_in1 = 6'b000101;
       # 100
       
       // test vector 3
+      //A is larger with carry
       test_in0 = 6'b101010;
       test_in1 = 6'b100101;
       # 100
       
       // test vector 4
+      //A is larger no carry
       test_in0 = 6'b101010;
       test_in1 = 6'b000101;
       # 100
       
       // test vector 5
+      //B is larger with carry
       test_in0 = 6'b100010;
       test_in1 = 6'b100011;
       # 100
       
       // test vector 6
+      //A abd B all bits high
       test_in0 = 6'b111111;
       test_in1 = 6'b111111;
       # 100
       
       // test vector 7
+      //output is all bits high
       test_in0 = 6'b101010;
       test_in1 = 6'b010101;
       # 100
       
       // test vector 8
+      //A and B small B is larger
       test_in0 = 6'b000010;
       test_in1 = 6'b000011;
       # 100
